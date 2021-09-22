965e6e8791065f899da288593bebc30894942cf8
Igor Solfa Romero <igorsolfaromero@gmail.com>
2021-09-22 11:29:48 -0300

    CRUD e Criando novas tabelas

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -8,7 +8,67 @@ class PessoaController {
         } catch (error) {
             return response.status(500).json(error.message);
         }
+    };
+
+    static async pegarUmaPessoa(request, response) {
+        const { id } = request.params;
+        try {
+            const umaPessoa = await database.Pessoas.findOne({
+                where: {
+                    id: Number(id)
+                }
+            });
+            return response.status(200).json(umaPessoa);
+        } catch (error) {
+            return response.status(500).json(error.message);
+        }
+    };
+
+    static async criarPessoa(request, response) {
+        const novaPessoa = request.body;
+        try {
+            const novaPessoaCriada = await database.Pessoas.create(novaPessoa);
+            return response.status(201).json(novaPessoaCriada);
+        } catch (error) {
+            return response.status(500).json(error.message);
+        }
+    };
+
+    static async atualizaPessoa(request, response) {
+        const { id } = request.params;
+        const novasInfos = request.body;
+        try {
+            await database.Pessoas.update(novasInfos,
+                {
+                    where: {
+                        id: Number(id)
+                    }
+                });
+
+            const pessoaAtualizada = await database.Pessoas.findOne(
+                {
+                    where: {
+                        id: Number(id)
+                    }
+                });
+
+            return response.status(200).json(pessoaAtualizada);
+        } catch (error) {
+            return response.status(500).json(error.message);
+        }
+    };
+
+    static async apagaPessoa(request, response) {
+        const { id } = request.params;
+        try {
+            await database.Pessoas.destroy({ where: { id: Number(id) } });
+            return response.status(200).json({ mensagem: `id ${id} deletado` });
+        } catch (error) {
+            return response.status(500).json(error.message);
+        }
     }
+
+
 }
 
 module.exports = PessoaController;
--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -5,4 +5,12 @@ const router = Router();
 
 router.get("/pessoas", PessoaController.pegaTodasAsPessoas);
 
+router.get("/pessoas/:id", PessoaController.pegarUmaPessoa);
+
+router.post("/pessoas", PessoaController.criarPessoa);
+
+router.put("/pessoas/:id", PessoaController.atualizaPessoa);
+
+router.delete("/pessoas/:id", PessoaController.apagaPessoa);
+
 module.exports = router;
