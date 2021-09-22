3db54162c7456833f762fda78cd7d1edec3f7994
Igor Solfa Romero <igorsolfaromero@gmail.com>
2021-09-22 14:08:30 -0300

    Associando e referenciando tabelas com Sequelize

--- api/models/pessoas.js
+++ api/models/pessoas.js
@@ -11,6 +11,12 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      Pessoas.hasMany(models.Turmas, {
+        foreignKey: 'docente_id'
+      });
+      Pessoas.hasMany(models.Matriculas, {
+        foreignKey: 'estudante_id'
+      });
     }
   };
   Pessoas.init({
