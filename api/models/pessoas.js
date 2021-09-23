d7bcb0336f9bfad461e39cddcea2905e8e8a8c5a
Igor Solfa Romero <igorsolfaromero@gmail.com>
2021-09-23 11:02:38 -0300

    validação no backend com sequelize

--- api/models/pessoas.js
+++ api/models/pessoas.js
@@ -20,9 +20,24 @@ module.exports = (sequelize, DataTypes) => {
     }
   };
   Pessoas.init({
-    nome: DataTypes.STRING,
+    nome: {
+      type: DataTypes.STRING,
+      validate: {
+        funcaoValidadora: function (dado) {
+          if (dado.length < 3) throw new Error("O campo nome deve ter mais de 3 caracteres.");
+        }
+      }
+    },
     ativo: DataTypes.BOOLEAN,
-    email: DataTypes.STRING,
+    email: {
+      type: DataTypes.STRING,
+      validate: {
+        isEmail: {
+          args: true,
+          msg: "Email inválido"
+        }
+      }
+    },
     role: DataTypes.STRING
   }, {
     sequelize,
